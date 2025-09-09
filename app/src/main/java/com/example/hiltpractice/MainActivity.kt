a050501c5c61a213e1662fe9f237f3893baa5b36
Prashant-Sgh <prashantsingh190804@gmail.com>
2025-09-09 23:47:27 +0530

    Relationship of hilt and interface

--- app/src/main/java/com/example/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/example/hiltpractice/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hiltpractice.ui.theme.HiltPracticeTheme
+import dagger.hilt.android.AndroidEntryPoint
+import jakarta.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
             HiltPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = someClass.getString(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,15 +40,38 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+class SomeClass @Inject constructor(
+    private val someInterfaceImp: SomeInterfaceImp
+)
+{
+    fun getString(): String {
+        return someInterfaceImp.hiltGreeting()
+    }
 }
 
-@Preview(showBackground = true)
+class SomeInterfaceImp @Inject constructor(): SomeInterface {
+    override fun hiltGreeting(): String {
+        return "Hey Hilt, returned this string.."
+    }
+}
+
+interface SomeInterface {
+    fun hiltGreeting() : String
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(
+            text = name,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     HiltPracticeTheme {

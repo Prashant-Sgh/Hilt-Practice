abc28cdaa22ab24dd0e8d7bba3efc1703a25e4bf
Prashant-Sgh <prashantsingh190804@gmail.com>
2025-09-17 12:46:13 +0530

    ...

--- app/src/main/java/com/example/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/example/hiltpractice/MainActivity.kt
@@ -1,35 +1,27 @@
 package com.example.hiltpractice
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.hiltpractice.ui.theme.HiltPracticeTheme
-import dagger.hilt.android.AndroidEntryPoint
-import jakarta.inject.Inject
 import androidx.core.net.toUri
+import com.example.hiltpractice.ui.theme.HiltPracticeTheme
 
 class MainActivity : ComponentActivity() {
 
